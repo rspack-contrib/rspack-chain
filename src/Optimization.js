0562ae8a232b290d2c341498d557c17c189dae3f
neverland <chenjiahan.jait@bytedance.com>
2025-12-17 17:01:03 +0800

    feat: add `inlineExports` option to optimization (#69)

--- src/Optimization.js
+++ src/Optimization.js
@@ -26,6 +26,7 @@ export default class extends ChainedMap {
       'portableRecords',
       'mangleExports',
       'innerGraph',
+      'inlineExports',
       'realContentHash',
       'avoidEntryIife',
     ]);
